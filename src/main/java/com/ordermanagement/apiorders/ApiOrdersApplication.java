d2bc88dc6a6e88cd2bad7af794eb27e40eee8e56
felipe <fa.ferreira@unesp.br>
2026-02-05 20:41:39 -0300

    Initial commit

---
+++ src/main/java/com/ordermanagement/apiorders/ApiOrdersApplication.java
@@ -0,0 +1,13 @@
+package com.ordermanagement.apiorders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiOrdersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiOrdersApplication.class, args);
+	}
+
+}
